db76c40dab2670e31ee029da9249abd72b70bc38
Diego Herrera <proyectos1@insepet.com>
2016-04-22 08:18:32 -0500

    GRP700 fleet. WEB

---
+++ customer.php
@@ -0,0 +1,50 @@
+<!--[if lt IE 7]> <html class="no-js ie6 oldie" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js ie7 oldie" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js ie8 oldie" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>GRP700 Fleet:: Sistemas Insepet :: Customer</title>
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<link rel="stylesheet" href="css/style.css">
+	<script src="js/libs/modernizr-2.0.6.min.js"></script>
+</head>
+    <body>
+        <div id="header-container">
+		<header class="wrapper clearfix">
+                    
+                    <h1 id="title"><a href="index.php"><img src="images/logo-insepet.png" ></a></h1>
+			<nav>
+				<ul>
+                                    <li><a href="customer.php">Customer</a></li>
+					<li><a href="#">Sales</a></li>
+					<li><a href="#">Setup</a></li>
+				</ul>
+			</nav>
+		</header>            
+	</div>
+        <div id="main-container">
+		<div id="main" class="wrapper clearfix">
+			
+			<article>
+				<header>
+					<h1>Customer Setup</h1>
+					<p></p>
+				</header>								
+			</article>
+			
+			<aside>
+				<h3>Account configuration</h3>
+				<p>In this section you can configure a new account and the associated vehicle information</p>
+			</aside>
+			
+		</div> <!-- #main -->
+	</div> <!-- #main-container -->
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,54 @@
+<!--[if lt IE 7]> <html class="no-js ie6 oldie" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js ie7 oldie" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js ie8 oldie" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>GRP700 Fleet:: Sistemas Insepet</title>
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<link rel="stylesheet" href="css/style.css">
+	<script src="js/libs/modernizr-2.0.6.min.js"></script>
+</head>
+    <body>
+        <div id="header-container">
+		<header class="wrapper clearfix">
+                    
+                    <h1 id="title"><a href="index.php"><img src="images/logo-insepet.png" ></a></h1>
+			<nav>
+				<ul>
+                                    <li><a href="customer.php">Customer</a></li>
+					<li><a href="#">Sales</a></li>
+					<li><a href="#">Setup</a></li>
+				</ul>
+			</nav>
+		</header>            
+	</div>
+        <div id="main-container">
+		<div id="main" class="wrapper clearfix">
+			
+			<article>
+				<header>
+					<h1>GRP700-Fleet</h1>
+					<p>Simple and effective Fleet control sistem. GRP700-Fleet allows
+                                           to control car fuel deliveries, customer fuel consumption, customer account
+                                           controls and dispenser basic configuration and control.</p>
+				</header>								
+			</article>
+			
+			<aside>
+				<h3>Simple and efficient</h3>
+				<p>You can create different accounts and use an ibutton identification system
+                                   or your preferred ID system. Reduce your administrative efford and increase your
+                                   profit.</p>
+			</aside>
+			
+		</div> <!-- #main -->
+	</div> <!-- #main-container -->
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
